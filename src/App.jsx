18e012151e6ab459fa0e2c1a53fd0cc15b1a87cf
bespoke-it-admin2023 <jomer.bespoke@gmail.com>
2023-02-17 07:58:07 +0800

    Rearrange Components

--- src/App.jsx
+++ src/App.jsx
@@ -20,9 +20,9 @@ function App() {
         <Hero />
       </div>
       <div>
-        <AboutUs />
         <OurServices />
         <HowitWorks />
+        <AboutUs />
       </div>
       <WhatsAppFloatingButton />
       <Footer />
