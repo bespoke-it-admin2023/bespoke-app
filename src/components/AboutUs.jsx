6a79e9b86c212f3aa74af4b065e58fd6b174cd1e
bespoke-it-admin2023 <jomer.bespoke@gmail.com>
2023-02-17 11:31:07 +0800

    Init

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,49 +1,41 @@
 import React from 'react'
-import UnStyledButton from './UnStyledButton'
-import securityicon from '../assets/Icons/securityicon.png'
-import kangaroowhite from '../assets/Icons/kangaroowhite.png'
-import kangarooaustralia from '../assets/Icons/kangarooaustralia.png'
-import cost0 from '../assets/Icons/cost0.png'
-import cost1 from '../assets/Icons/cost1.png'
-import scale0 from '../assets/Icons/scale0.png'
-import scale1 from '../assets/Icons/scale1.png'
-import leverage0 from '../assets/Icons/leverage0.png'
-import leverage1 from '../assets/Icons/leverage1.png'
-import talent0 from '../assets/Icons/talent0.png'
-import talent1 from '../assets/Icons/talent1.png'
-import DynamicCard from './DynamicCard'
-
+import aboutus from '../assets/aboutus.png'
+import gradientbox from '../assets/gradient-box.png'
 
 const AboutUs = () => {
   return (
-    <section id="aboutus">
-        <div className="w-full h-full flex flex-row bg-gradient-to-b from-indigo-300 to-cyan-200 justify-evenly">
-            <div className="flex flex-col pt-12 pl-5 max-w-[900px] w-900px pb-5">
-                <span className="w-[3/4] h-full text-[25px] pb-4">
-                    With the constant advancements in technology, you can be sure the data we <br/>process for you is secured in our in-house dedicated server under lock and key <br/>with 24 hours surveillance monitoring.
-                    <br/>
-                    <br/>
-                    Our stringent measures that we have set in stone on all our Bespoke staff are<br/> implemented to the highest standard to prevent any data leak as we fully <br/>understand these sensitive data are proprietary to our customers only.
-                </span>
-                <UnStyledButton text="Find out more" buttonStyle="bg-brandBlue text-white rounded-full p-4 hover:bg-brandBlack font-semibold font-poppins" redirect="" />
+    <div className="flex flex-col overflow-hidden justify-center">
+        <div className="flex flex-col justify-center">
+            <img src={ aboutus } alt="aboutus" className="w-auto h-auto"/>
+            <div className="flex justify-center py-5">
+                <h1 className="text-brandBlue font-poppins font-semibold text-[30px]">WHO WE ARE</h1>
             </div>
-            <div className='items-center flex justify-center'>
-                <img src={securityicon} alt="securityicon" className='w-[360px] h-[380px] items-center'/>
+            <span className="flex justify-center text-center py-5 px-[10%]">
+            Bespoke is led by a team of professionals that have at least 20 years combined experience in the freight forwarding industry in Australia. We are well versed with the importation and exportation requirements that are unique to Australia. Be it the rules and regulations of AQIS (Australian Quarantine and Inspection Scheme) or the daily operations of the various departments such as the operations / forwarding team to the cartage department and the customs broker department, we have done it all. Do drop us a quick enquiry of how we can be of service to you on your staffing requirements. We are certain we would even be able to advise you on building a team of staff that can generate more income for your business that you didn't know you needed.
+            </span>    
+        </div>
+        <div className="flex flex-row px-[10%]">
+            <img src={gradientbox} alt="box" className="w-[40%] h-auto"/>
+            <div className="flex flex-col justify-center text-center">
+                <h1 className="font-poppins font-semibold text-brandBlue text-[30px] py-2">BESPOKE MISSION</h1>
+                <span className="px-[20%]">We work towards operating in a manner where our client's are satisfied that their business processes are met with the highest standard of service and effort by the top class talents that we hire.</span>
             </div>
         </div>
-        <div className="flex flex-col justify-center items-center px-10 py-2">
-            <h1 className="font-poppins font-bold text-brandBlue text-[35px]">
-                HOW YOUR BESPOKE STAFF CAN HELP WITH YOUR BUSINESS
-            </h1>
-            <div className="flex flex-row overflow-hidden bg-slate-200 rounded-[55px] w-[full] h-[500px]">
-                <DynamicCard image0={ kangaroowhite } image1={ kangarooaustralia }  text="Australian reps at your beck and call"/>
-                <DynamicCard image0={ scale1 } image1={ scale0 } text="Scale faster with efficiency"/>
-                <DynamicCard image0={ cost1 } image1={ cost0 } text="Reduce staffing cost"/>
-                <DynamicCard image0={ leverage1 } image1={ leverage0 } text="Leverage your productivity to focus on what you need to the most"/>
-                <DynamicCard image0={ talent1 } image1={ talent0 } text="Widen Talent search"/>
+        <div className="flex flex-row px-[10%]">
+            <div className="flex flex-col justify-center text-center">
+                <h1 className="font-poppins font-semibold text-brandBlue text-[30px] py-2">BESPOKE VISION</h1>
+                <span className="px-[20%]">We believe in quality over quantity. Therefore we ensure that we get the best person to do the job right with no expense spared on the constant upgrade of workforce skills, technology and productivity.</span>
+            </div>
+            <img src={gradientbox} alt="box" className="w-[40%] h-auto transform rotate-180"/>
+        </div>
+        <div className="flex flex-row px-[10%]">
+            <img src={gradientbox} alt="box" className="w-[40%] h-auto"/>
+            <div className="flex flex-col justify-center text-center">
+                <h1 className="font-poppins font-semibold text-brandBlue text-[30px] py-2">BESPOKE CORE VALUES</h1>
+                <span className="px-[20%]">We strive to maintain our integrity whenever it comes to fairness in the workplace, stellar productivity on the services we provide and securing the data that we process on behalf of our customers.</span>
             </div>
         </div>
-    </section>
+    </div>
   )
 }
 
