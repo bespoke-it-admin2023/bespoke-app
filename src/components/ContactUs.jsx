05dd900cad5e70543757d60f0577f2bef8b5f0ce
bespoke-it-admin2023 <jomer.bespoke@gmail.com>
2023-02-17 09:52:11 +0800

    Added a hover effect on text to turn to brandBlue

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ContactUs = () => {
     return (
         <a href="/contactus">
-            <button className="w-[1/6] max-h-[50px] bg-transparent text-black rounded-full border-black border-2 ml-10 p-3 cursor-pointer font-bold">
+            <button className="w-[1/6] max-h-[50px] bg-transparent text-black rounded-full border-black border-2 hover:text-brandBlue ml-10 p-3 cursor-pointer font-bold">
                 CONTACT US
             </button>
         </a>
