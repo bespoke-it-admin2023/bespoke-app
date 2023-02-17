4f10263218266f3176e3bbdd9fa6ceb5655e83d4
bespoke-it-admin2023 <jomer.bespoke@gmail.com>
2023-02-17 09:52:56 +0800

    Added framer-motion animations

--- src/components/UnStyledButton.jsx
+++ src/components/UnStyledButton.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
-
+import { motion } from 'framer-motion'
 
 const UnStyledButton = ({text, redirect, buttonStyle}) => {
   return (
     <div className="text-[25px] pb-5">
         <a href={ redirect }>
-            <button className={ buttonStyle }>
+            <motion.button 
+              className={ buttonStyle }
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              >
                 {text}
-            </button>
+            </motion.button>
         </a>
     </div>
   )
